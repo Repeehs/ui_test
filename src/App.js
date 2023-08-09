3f1e7f04f28eb7a696ff6fcc53652c1ca3d6d707
MaiVanBinh <55869650+MaiVanBinh@users.noreply.github.com>
2023-08-09 16:27:35 +0700

    d

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./component/base/Layout";
 import Books from "./pages/books";
+import SignIn from "./pages/signIn";
 
 function App() {
   return (
     <>
-      <Layout>
-        <Books />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route index element={<Books />} />
+            <Route path="/sign-in" element={<SignIn />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </>
   );
 }
--- src/component/base/AppHeader/index.js
+++ src/component/base/AppHeader/index.js
@@ -1,23 +1,29 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const drawerWidth = 240;
-const navItems = ['Dang Nhap'];
+const navItems = [
+  {
+    title: "Dang Nhap",
+    url: "/sign-in",
+  },
+];
 
-const AppHeader = (props)  => {
+const AppHeader = (props) => {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -26,56 +32,64 @@ const AppHeader = (props)  => {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <Typography variant="h6" sx={{ my: 2 }}>
         MUI
       </Typography>
       <Divider />
       <List>
         {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
+          <Link to={item.url}>
+            <ListItem key={item.title} disablePadding>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item.title} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
         ))}
       </List>
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" sx={{ position: 'relative', background: '#6b9876' }}>
+      <AppBar
+        component="nav"
+        sx={{ position: "relative", background: "#6b9876" }}
+      >
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            S3LAP
+            <Link to="/">S3LAP</Link>
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
-              </Button>
+              <Link to={item.url}>
+                <Button key={item.title} sx={{ color: "#fff" }}>
+                  {item.title}
+                </Button>
+              </Link>
             ))}
           </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav" >
+      <Box component="nav">
         <Drawer
           container={container}
           variant="temporary"
@@ -85,8 +99,11 @@ const AppHeader = (props)  => {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
@@ -94,6 +111,6 @@ const AppHeader = (props)  => {
       </Box>
     </Box>
   );
-}
+};
 
 export default AppHeader;
